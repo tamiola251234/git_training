b5bfd9869b4e875cc139fae40797eb70355ca4d3
Klaudiusz Tamioła <tamiola.klaudiusz@gmail.com>
2023-10-27 09:07:52 +0200

    zmiana mnozenia w metodzie Policz

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int Policz(int a, int b) {
+    return a*b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu; 2+3=%d", Policz(2, 3));
     return 0;
 }
